a1623efaee026efa06ab96d04462cccb84060b9e
ValentineCodes <Valentineorga@gmail.com>
2023-05-05 22:07:58 +0100

    bug fixes

--- packages/hardhat/test/unit/snowman.spec.ts
+++ packages/hardhat/test/unit/snowman.spec.ts
@@ -1,10 +1,13 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
 import { deployments, ethers } from "hardhat";
-import { Snowman } from "../../typechain-types";
+import { Hat, Snowman } from "../../typechain-types";
 import { BigNumber } from "ethers";
 
 describe("Snowman☃️", () => {
+  const SNOWMAN_MINT_FEE: BigNumber = ethers.utils.parseEther("0.02");
+  const HAT_MINT_FEE: BigNumber = ethers.utils.parseEther("0.01");
+
   let owner: SignerWithAddress;
   let valentine: SignerWithAddress;
 
@@ -27,15 +30,35 @@ describe("Snowman☃️", () => {
       const feeCollector: string = await snowman.getFeeCollector();
       const oldFeeCollectorBalance: BigNumber = await ethers.provider.getBalance(feeCollector);
       console.log("Old Balance: ", ethers.utils.formatEther(oldFeeCollectorBalance), "💎");
-      const MINT_FEE: BigNumber = ethers.utils.parseEther("0.02");
 
       console.log("Minting One(1) Snowman☃️...");
-      await snowman.mint({ value: MINT_FEE });
+      await snowman.mint({ value: SNOWMAN_MINT_FEE });
       console.log(`Successfully Minted One(1) Snowman☃️`);
 
       const newFeeCollectorBalance: BigNumber = await ethers.provider.getBalance(feeCollector);
       console.log("New Balance: ", ethers.utils.formatEther(newFeeCollectorBalance), "💎");
-      expect(newFeeCollectorBalance).to.equal(oldFeeCollectorBalance.add(MINT_FEE));
+      expect(newFeeCollectorBalance).to.equal(oldFeeCollectorBalance.add(SNOWMAN_MINT_FEE));
+    });
+  });
+
+  describe("💬addAccessory", () => {
+    it("adds other nfts to the snowman for composition", async () => {
+      await snowman.mint({ value: SNOWMAN_MINT_FEE });
+      console.log("Snowman minted✅");
+
+      await deployments.fixture("Hat");
+
+      const hat: Hat = await ethers.getContract("Hat", valentine);
+      await hat.mint({ value: HAT_MINT_FEE });
+
+      expect((await hat.ownerOf(1)).toLowerCase()).to.equal(valentine.address.toLowerCase());
+      console.log("Hat minted✅");
+
+      await snowman.connect(owner).addAccessory(hat.address, 1);
+      console.log("Hat added as an accessory✅");
+
+      const snowmanId = ethers.utils.defaultAbiCoder.encode(["uint256"], [1]);
+      await hat["safeTransferFrom(address,address,uint256,bytes)"](valentine.address, snowman.address, 1, snowmanId);
     });
   });
 });
