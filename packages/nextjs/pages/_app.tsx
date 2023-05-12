4b4da1cac21118963e2b8585eee057e9f6752b0a
ValentineCodes <Valentineorga@gmail.com>
2023-05-12 17:58:22 +0100

    implemented minting functionality

--- packages/nextjs/pages/_app.tsx
+++ packages/nextjs/pages/_app.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import type { AppProps } from "next/app";
 import { RainbowKitProvider, darkTheme, lightTheme } from "@rainbow-me/rainbowkit";
 import "@rainbow-me/rainbowkit/styles.css";
+import { ChakraBaseProvider, extendBaseTheme } from '@chakra-ui/react'
+import chakraTheme from '@chakra-ui/theme'
 import NextNProgress from "nextjs-progressbar";
 import { Toaster } from "react-hot-toast";
 import { useDarkMode } from "usehooks-ts";
@@ -20,6 +22,14 @@ const ScaffoldEthApp = ({ Component, pageProps }: AppProps) => {
   // This variable is required for initial client side rendering of correct theme for RainbowKit
   const [isDarkTheme, setIsDarkTheme] = useState(true);
   const { isDarkMode } = useDarkMode();
+
+  const { Spinner } = chakraTheme.components
+
+const theme = extendBaseTheme({
+  components: {
+    Spinner,
+  },
+})
 
   useEffect(() => {
     if (price > 0) {
@@ -41,9 +51,12 @@ const ScaffoldEthApp = ({ Component, pageProps }: AppProps) => {
       >
         <div className="flex flex-col min-h-screen">
           <Header />
+          <ChakraBaseProvider theme={theme}>
           <main className="relative flex flex-col flex-1">
             <Component {...pageProps} />
           </main>
+
+          </ChakraBaseProvider>
         </div>
         <Toaster />
       </RainbowKitProvider>
--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -3,14 +3,23 @@ import type { NextPage } from "next";
 import Link from "next/link";
 
 import { useTypewriter } from 'react-simple-typewriter'
+import { Spinner } from '@chakra-ui/react'
 
 import Snowman from "~~/components/cards/Snowman";
+import { useScaffoldContractWrite, useTransactor } from "~~/hooks/scaffold-eth";
+import { notification } from "~~/utils/scaffold-eth";
 
 const Home: NextPage = () => {
   const [animatedText] = useTypewriter({
     words: ['Snowman?', 'Half-Assed Olaf?🫡'],
     loop: 0,
     delaySpeed: 2500
+  })
+
+  const {writeAsync: mint, isLoading: isMinting, isSuccess: isMinted} = useScaffoldContractWrite({
+    contractName: "Snowman",
+    functionName: "mint",
+    value: "0.02"
   })
 
   return (
@@ -23,10 +32,10 @@ const Home: NextPage = () => {
     <main className="flex flex-col items-center gap-4 px-4 py-12">
       <h2 className="text-[1.8rem] md:text-[2.5rem] text-center h-16 md:h-20">Do you wanna build a <br /><strong className="text-orange-500">{animatedText}</strong></h2>
       <p className="text-md md:text-xl -mt-2 text-center max-w-lg">Mint a unique Snowman☃️ for 0.02 ETH and compose with a bunch of <Link href="/" className="underline transition duration-300 text-orange-300 hover:text-orange-500 font-bold">accessories</Link>🎩🧣</p>
-      <button className="border-orange-500 bg-orange-500 hover:border-black hover:bg-white hover:text-black transition-all px-4 py-2 text-white rounded-md shadow-lg">Mint One</button>
+      <button className="border-orange-500 bg-orange-500 hover:border-black hover:bg-white hover:text-black transition-all px-4 py-2 text-white rounded-md shadow-lg" onClick={mint} disabled={isMinting}>{isMinting? <Spinner size="md" />: "Mint One"}</button>
 
       <div className="flex flex-wrap justify-center items-center gap-5 my-10">
-       {[1,2,3,4,5].map(i => <Snowman />)}
+       {[1,2,3].map(i => <Snowman />)}
       </div>
     </main>
     </>
